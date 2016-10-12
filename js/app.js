781314106d8fede69d39aca5cc763489400bc57f
Isaacjcollier <isaacjcollier@gmail.com>
2016-10-12 14:00:00 +0100

    init

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 (function() {
   'use strict';
 
+  var app = angular.module('cameraApp', []);
+
+  app.filter('astericks', function () {
+    return (input) => {
+      var astericksRating = [];
+      for (var i = 0; i < input; i++) {
+        astericksRating.push('*');
+      }
+      return astericksRating.join('');
+    }
+  })
+
+
   $('select').material_select();
 
 })();
